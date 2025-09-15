eca8816537dac58d9b0266179f27328c51a8a27a
Anisha Chhajer <anishachhajer16@gmail.com>
2025-09-15 18:34:22 +0530

    C language Assignments

---
+++ C_Assignments/loop/q1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int i=10;i<=50;i++)
+    {
+        // if(i%2!=0)
+        if(i%2==0)
+        {
+        // printf("%d \n",i);
+        }
+        else
+        {
+            printf("%d \n",i);
+        }
+    }
+
+    return 0;
+}
+
