2f646a73a0ab1ea5386520988497559438e8f232
Anisha Chhajer <anishachhajer16@gmail.com>
2025-09-20 12:20:40 +0530

    Assignment submission

---
+++ C_Assignments/Pattern/q36.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a no.-");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<(i+1);j++){ 
+            printf(" ");                                 
+        }
+            for(int k=0;k<=i;k++){
+                printf("%c",'A'+k);
+            }
+        printf("\n");
+    }
+    return 0;
+}
