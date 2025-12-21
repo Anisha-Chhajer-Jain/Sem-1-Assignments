e5fd7ddd0986b2b7207ece18793eb3ce74f46348
Anisha Chhajer <anishachhajer16@gmail.com>
2025-12-22 00:54:35 +0530

    sem-1 assignment update

--- JAVASCRIPT_Assignments/04.js
+++ JAVASCRIPT_Assignments/04.js
@@ -3,5 +3,7 @@ if(age<0)
 console.log("Invalid Age");
 else if(age<18)
 console.log("Not Eligible to vote");
+else if(age>90)
+    console.log("maara hi nhi")
 else 
 console.log("Eligible to vote");
