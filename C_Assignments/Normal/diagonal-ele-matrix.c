d846b3d25030fcc073e5e93f13c6ca1c73323fe4
Anisha Chhajer <anishachhajer16@gmail.com>
2026-01-16 21:56:01 +0530

    diagonal matrix

--- C_Assignments/Normal/diagonal-ele-matrix.c
+++ C_Assignments/Normal/diagonal-ele-matrix.c
@@ -1,46 +1,46 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main()
-// {
-//     int n, m;
+int main()
+{
+    int n, m;
 
-//     printf("=== [INPUT] ===");
+    printf("=== [INPUT] ===");
 
-//     printf("\nEnter the row count (n): ");
-//     scanf("%d", &n);
+    printf("\nEnter the row count (n): ");
+    scanf("%d", &n);
 
-//     printf("Enter the column count (m): ");
-//     scanf("%d", &m);
+    printf("Enter the column count (m): ");
+    scanf("%d", &m);
 
-//     int a[n][m];
-//     printf("\n---Enter the elements of the array---\n");
-//     for(int i = 0; i < n; i++)
-//     {
-//         for(int j  = 0; j < m; j++)
-//         {
-//             printf("-Enter element at (%d, %d): ", i, j);
-//             scanf("%d", &a[i][j]);
-//         }
-//     }
+    int a[n][m];
+    printf("\n---Enter the elements of the array---\n");
+    for(int i = 0; i < n; i++)
+    {
+        for(int j  = 0; j < m; j++)
+        {
+            printf("-Enter element at (%d, %d): ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
     
-//     printf("\n=== [OUTPUT] ===");
+    printf("\n=== [OUTPUT] ===");
 
-//     int diagSum = 0;
-//     printf("\nDiagonal Elements: {");
-//     for(int i = 0; i < n; i++)
-//     {
-//         for(int j  = 0; j < m; j++)
-//         {
-//             if(i != j) continue;
+    int diagSum = 0;
+    printf("\nDiagonal Elements: {");
+    for(int i = 0; i < n; i++)
+    {
+        for(int j  = 0; j < m; j++)
+        {
+            if(i != j) continue;
 
-//             //If it is a diagonal element
-//             diagSum += a[i][j];
-//             printf("%s%d", i == 0 ? "" : ", ", a[i][j]);
-//         }
-//     }
+            //If it is a diagonal element
+            diagSum += a[i][j];
+            printf("%s%d", i == 0 ? "" : ", ", a[i][j]);
+        }
+    }
 
-//     printf("}");
-//     printf("\nSum of diagonal elements: %d", diagSum);
+    printf("}");
+    printf("\nSum of diagonal elements: %d", diagSum);
 
-//     return 0;
-// }
+    return 0;
+}
