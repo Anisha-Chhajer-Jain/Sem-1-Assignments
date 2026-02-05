0e09b1455b61e1824cdb94a09321c4a709646f6a
Anisha Chhajer <anishachhajer16@gmail.com>
2026-02-05 14:03:09 +0530

    swap pointers

--- C_Assignments/Normal/swappointers.c
+++ C_Assignments/Normal/swappointers.c
@@ -1,59 +1,59 @@
-// //Write a C program to swap two arrays using pointers.
-// #include <stdio.h>
+//Write a C program to swap two arrays using pointers.
+#include <stdio.h>
 
-// void populateArray(char *name, int array[], int size)
-// {
-//     printf("\n\n=== Populating Array '%s' ===", name);
+void populateArray(char *name, int array[], int size)
+{
+    printf("\n\n=== Populating Array '%s' ===", name);
 
-//     printf("\n---Enter the elements of the array---\n");
-//     for(int i = 0; i < size; i++)
-//     {
-//         printf("Enter the element at (%d): ", i);
-//         scanf("%d", array + i);
-//     }
-// }
+    printf("\n---Enter the elements of the array---\n");
+    for(int i = 0; i < size; i++)
+    {
+        printf("Enter the element at (%d): ", i);
+        scanf("%d", array + i);
+    }
+}
 
-// void swapElements(int array1[], int array2[], int size)
-// {
-//     for(int i = 0; i < size; i++)
-//     {
-//         int temp = *(array1 + i);
-//         *(array1 + i) = *(array2 + i);
-//         *(array2 + i) = temp; 
-//     }
-// }
+void swapElements(int array1[], int array2[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        int temp = *(array1 + i);
+        *(array1 + i) = *(array2 + i);
+        *(array2 + i) = temp; 
+    }
+}
 
-// void displayArray(char *name, int array[], int size)
-// {
-//     printf("\nArray '%s': [", name);
+void displayArray(char *name, int array[], int size)
+{
+    printf("\nArray '%s': [", name);
 
-//     for(int i = 0; i < size; i++)
-//         printf("%d%s", *(array + i), i == size - 1 ? "" : ", ");
+    for(int i = 0; i < size; i++)
+        printf("%d%s", *(array + i), i == size - 1 ? "" : ", ");
 
-//     printf("]");
-// }
+    printf("]");
+}
 
-// int main()
-// {
-//     int size;
+int main()
+{
+    int size;
 
-//     printf("=== [INPUT] ===");
-//     printf("\nEnter the size of the arrays: ");
-//     scanf("%d", &size);
+    printf("=== [INPUT] ===");
+    printf("\nEnter the size of the arrays: ");
+    scanf("%d", &size);
 
-//     int a[size];
-//     int b[size];
+    int a[size];
+    int b[size];
 
-//     populateArray("A (1st Array)", a, size);
-//     displayArray("A", a, size);
+    populateArray("A (1st Array)", a, size);
+    displayArray("A", a, size);
 
-//     populateArray("B (2nd Array)", b, size);
-//     displayArray("B", b, size);
+    populateArray("B (2nd Array)", b, size);
+    displayArray("B", b, size);
 
-//     printf("\n\n=== [OUTPUT] ===");
-//     swapElements(a, b, size);
-//     displayArray("A", a, size);
-//     displayArray("B", b, size);
+    printf("\n\n=== [OUTPUT] ===");
+    swapElements(a, b, size);
+    displayArray("A", a, size);
+    displayArray("B", b, size);
 
-//     return 0;
-// }
+    return 0;
+}
