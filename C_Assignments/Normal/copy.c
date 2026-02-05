5cfd8f50b12eba42f602cee89b0a0231b744ba84
Anisha Chhajer <anishachhajer16@gmail.com>
2026-02-05 14:01:25 +0530

    copy

--- C_Assignments/Normal/copy.c
+++ C_Assignments/Normal/copy.c
@@ -1,35 +1,35 @@
-// //Write a C program to copy one array to other using pointers.
-// #include <stdio.h>
+//Write a C program to copy one array to other using pointers.
+#include <stdio.h>
 
-// void copyValues(int sourceArray[], int targetArray[], int elementCount)
-// {
-//     for(int i = 0; i < elementCount; i++)
-//         *(targetArray + i) = *(sourceArray + i); 
-// }
+void copyValues(int sourceArray[], int targetArray[], int elementCount)
+{
+    for(int i = 0; i < elementCount; i++)
+        *(targetArray + i) = *(sourceArray + i); 
+}
 
-// int main()
-// {
-//     int n;
+int main()
+{
+    int n;
 
-//     printf("=== [INPUT] ===");
-//     printf("\nEnter the size of the array: ");
-//     scanf("%d", &n);
+    printf("=== [INPUT] ===");
+    printf("\nEnter the size of the array: ");
+    scanf("%d", &n);
 
-//     int a[n];
+    int a[n];
 
-//     printf("\n---Enter the elements of the array---\n");
-//     for(int i = 0; i < n; i++)
-//     {
-//         printf("Enter the element at (%d): ", i);
-//         scanf("%d", a + i);
-//     }
+    printf("\n---Enter the elements of the array---\n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("Enter the element at (%d): ", i);
+        scanf("%d", a + i);
+    }
 
-//     int b[n];
-//     copyValues(a, b, n);
+    int b[n];
+    copyValues(a, b, n);
 
-//     printf("\n=== [OUTPUT] ===\n");
-//     for(int i = 0; i < n; i++)
-//         printf("%d ", *(b + i));
+    printf("\n=== [OUTPUT] ===\n");
+    for(int i = 0; i < n; i++)
+        printf("%d ", *(b + i));
 
-//     return 0;
-// }
+    return 0;
+}
