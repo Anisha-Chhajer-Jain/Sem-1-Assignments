d0a6d88049dc9b67c1aaf748ac8cbcccad20761b
Anisha Chhajer <anishachhajer16@gmail.com>
2026-02-05 14:01:57 +0530

    swap pointer

--- C_Assignments/Normal/normalswappointers.c
+++ C_Assignments/Normal/normalswappointers.c
@@ -1,31 +1,31 @@
-// //Write a C program to swap two numbers using pointers
-// #include <stdio.h>
+//Write a C program to swap two numbers using pointers
+#include <stdio.h>
 
-// void swap(int *a, int *b)
-// {
-//     int temp = *a;
-//     *a = *b;
-//     *b = temp;
-// }
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-// int main()
-// {
-//     int a;
-//     int b;
+int main()
+{
+    int a;
+    int b;
 
-//     int *pA = &a;
-//     int *pB = &b;
+    int *pA = &a;
+    int *pB = &b;
 
-//     printf("=== [INPUT] ===");
-//     printf("\nEnter the value of A: ");
-//     scanf("%d", pA);
+    printf("=== [INPUT] ===");
+    printf("\nEnter the value of A: ");
+    scanf("%d", pA);
 
-//     printf("Enter the value of B: ");
-//     scanf("%d", pB);
+    printf("Enter the value of B: ");
+    scanf("%d", pB);
 
-//     swap(pA, pB);
+    swap(pA, pB);
 
-//     printf("\n=== [OUTPUT] ===");
-//     printf("\nA: %d, B: %d", a, b);
-//     return 0;
-// }
+    printf("\n=== [OUTPUT] ===");
+    printf("\nA: %d, B: %d", a, b);
+    return 0;
+}
